68c5ea28884c28ed81eab5dbef3af2fde0e5638e
Luiz <luizabadi123@outlook.com>
2023-08-27 03:13:31 -0300

    Adicionar arquivos de projeto.

---
+++ APIconcessionaria/Models/Carro.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Teste1.Models
+{
+    public class Carro
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Ano { get; set; }
+        public string?  Combustivel { get; set; }
+        public string? Cor { get; set; }
+        public string? Motor { get; set; }
+        public int Potencia { get; set; }
+    }
+}
---
+++ APIconcessionaria/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Teste1.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string? NomeUsuario { get; set; }
+        public string? Senha { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
